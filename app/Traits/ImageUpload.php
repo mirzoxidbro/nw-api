b803868cc8eabbb20fc526dcc36d9f542b2b8771
mirzoxidbro <mirzoxidadxamjanov01@gmail.com>
2023-01-11 13:42:20 +0500

    image upload

---
+++ app/Traits/ImageUpload.php
@@ -0,0 +1,20 @@
+    <?php
+namespace App\Traits;
+
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+
+trait ImageUpload
+{
+    public function ImageUpload($query,$id) // Taking input image as parameter
+    {
+        $image_name = $id;
+        $ext = strtolower($query->getClientOriginalExtension()); // You can use also getClientOriginalName()
+        $image_full_name = $image_name.'.'.$ext;
+        $image_url = $image_full_name;
+        $success = $query->move(public_path('assets/image'),$image_full_name);
+
+        return $image_url; // Just return image
+    }
+}
+?>
