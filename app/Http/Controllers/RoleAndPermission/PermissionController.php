bb9dd4c171269cfa92280d0448eddf46ca9e44c4
mirzoxidbro <mirzoxidadxamjanov01@gmail.com>
2023-01-11 16:29:29 +0500

    get permission

--- app/Http/Controllers/RoleAndPermission/PermissionController.php
+++ app/Http/Controllers/RoleAndPermission/PermissionController.php
@@ -4,8 +4,12 @@ namespace App\Http\Controllers\RoleAndPermission;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Spatie\Permission\Models\Permission;
 
 class PermissionController extends Controller
 {
-    //
+    public function getPermissions()
+    {
+        return Permission::query()->get();
+    }
 }
