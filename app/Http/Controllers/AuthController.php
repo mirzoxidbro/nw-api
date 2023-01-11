b974000ba74233ce1556dd8046d920ebd8e669ef
mirzoxidbro <mirzoxidadxamjanov01@gmail.com>
2023-01-11 15:04:14 +0500

    authorization

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\User\UserRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Response;
+
+class AuthController extends Controller
+{
+    public function register(UserRequest $request)
+    {
+        $user = User::create([
+            "name" => $request->name,
+            "email" => $request->email,
+            "password" => Hash::make($request->password)
+        ]);
+
+        $token = $user->createToken("MyApp")->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token
+        ]);
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only(['email', 'password']);
+
+        if(Auth::attempt($credentials)){
+            $user = Auth::user(); 
+            $success['token'] =  $user->createToken('MyApp')->plainTextToken; 
+            $success['name'] =  $user->name;
+   
+            return response()->json([
+                $success
+            ]);
+        }
+
+        else{ 
+            return response()->json(['error'=>'Unauthorized']);
+        }
+    }
+
+    public function logout()
+    {
+        auth()->user()->tokens()->delete();
+        return response()->json([
+            "Log out"
+        ]);
+    }
+}
