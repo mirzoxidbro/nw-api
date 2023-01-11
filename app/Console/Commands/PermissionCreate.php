7f1474af872adb3d873869233bf85c0f1654c733
mirzoxidbro <mirzoxidadxamjanov01@gmail.com>
2023-01-11 16:08:45 +0500

    permission command

---
+++ app/Console/Commands/PermissionCreate.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class PermissionCreate extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'create:permission';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create Permission';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        /** User permission */
+    }
+}
