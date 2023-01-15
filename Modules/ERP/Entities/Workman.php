d5aeb00c3c37c3e15abc85a9e97a4593fd19ff9f
mirzoxidbro <mirzoxidadxamjanov01@gmail.com>
2023-01-15 18:32:13 +0500

    attribute

--- Modules/ERP/Entities/Workman.php
+++ Modules/ERP/Entities/Workman.php
@@ -2,6 +2,7 @@
 
 namespace Modules\ERP\Entities;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -15,5 +16,13 @@ class Workman extends Model
     public function attendances(): BelongsToMany
     {
         return $this->belongsToMany(Attendace::class, 'attendance_worker', 'worker_id', 'attendance_id');
+    }
+
+    protected function isArchived(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value) => $value == '0' ? 'false' : 'true',
+            set: fn($value) => $value == true ? 1 : 0
+        );
     }
 }
