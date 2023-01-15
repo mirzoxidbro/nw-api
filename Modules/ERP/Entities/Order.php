a2b41f3936892a4ff12d2a9156d05f0c8e7dc020
mirzoxidbro <mirzoxidadxamjanov01@gmail.com>
2023-01-15 22:31:24 +0500

    update at for humans

--- Modules/ERP/Entities/Order.php
+++ Modules/ERP/Entities/Order.php
@@ -3,8 +3,10 @@
 namespace Modules\ERP\Entities;
 
 use App\Models\User;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Carbon;
 
 class Order extends Model
 {
@@ -27,5 +29,12 @@ class Order extends Model
     public function getUsernameAttribute()
     {
         return User::query()->select('name')->find($this->user_id);
+    }
+
+    protected function updatedAt(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => Carbon::parse($value)->diffForHumans(),
+        );
     }
 }
