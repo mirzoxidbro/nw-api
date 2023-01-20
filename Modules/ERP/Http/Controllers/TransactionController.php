0ade6d353ed8510a0f251df7d0f9e60c1193e799
mirzoxidbro <mirzoxidadxamjanov01@gmail.com>
2023-01-20 19:56:28 +0500

    transaction type

--- Modules/ERP/Http/Controllers/TransactionController.php
+++ Modules/ERP/Http/Controllers/TransactionController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Modules\ERP\Entities\Order;
 use Modules\ERP\Entities\PaymentPurpose;
 use Modules\ERP\Entities\Transaction;
 use Modules\ERP\Entities\Workman;
@@ -15,15 +16,24 @@ class TransactionController extends Controller
     public function store(Request $request)
     {
         $user = User::find($request->payer_id);
-        $worker = Workman::find($request->receiver_id);
         $purpose = PaymentPurpose::find($request->purpose_id);
         $model = new Transaction();
         $model->amount = $request->amount;
         $model->description = $request->description;
-        $model->payer()->associate($user);
-        $model->receiver()->associate($worker);
-        $model->purpose()->associate($purpose);
-        $model->save();
+        if($request->type == 'expense'){
+            $worker = Workman::find($request->receiver_id);
+            $model->payer()->associate($user);
+            $model->receiver()->associate($worker);
+            $model->purpose()->associate($purpose);
+            $model->save();
+        }else{
+            $order = Order::find($request->payer_id);
+            $model->receiver()->associate($user);
+            $model->payer()->associate($order);
+            $model->purpose()->associate($purpose);
+            $model->save();
+            
+        }
         return $model;
     }
 }
