5a6888515eba332da541fc447b0ef9491bbf9f5a
mirzoxidbro <mirzoxidadxamjanov01@gmail.com>
2023-01-21 19:17:22 +0500

    courier wallet

---
+++ Modules/ERP/Http/Controllers/CourierWalletController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Modules\ERP\Http\Controllers;
+
+use Illuminate\Routing\Controller;
+
+class CourierWalletController extends Controller
+{
+    public function index()
+    {
+        
+    }    
+}
