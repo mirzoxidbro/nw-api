15d903b72bfca1995d1d5cd2865c3c286d144d9f
mirzoxidbro <mirzoxidadxamjanov01@gmail.com>
2023-01-27 19:34:39 +0500

    fix

--- Modules/ERP/Repository/WorkmanRepository.php
+++ Modules/ERP/Repository/WorkmanRepository.php
@@ -10,7 +10,7 @@ class WorkmanRepository implements WorkmanRepositoryInterface
 {
     public function getWorkmans()
     {
-        return WorkmanResource::collection(Workman::select('name', 'phone', 'is_archived', 'updated_at')->paginate(10))->resource;
+        return WorkmanResource::collection(Workman::select('id', 'name', 'phone', 'is_archived', 'updated_at')->paginate(10))->resource;
     }
 
     public function save(array $data)
