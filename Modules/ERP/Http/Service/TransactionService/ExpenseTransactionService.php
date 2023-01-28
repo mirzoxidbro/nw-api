0ccba491e651c4eaa3868b282c07e5e9f294d701
mirzoxidbro <mirzoxidadxamjanov01@gmail.com>
2023-01-28 15:01:01 +0500

    fixed

--- Modules/ERP/Http/Service/TransactionService/ExpenseTransactionService.php
+++ Modules/ERP/Http/Service/TransactionService/ExpenseTransactionService.php
@@ -5,6 +5,7 @@ namespace Modules\ERP\Http\Service\TransactionService;
 use App\Models\User;
 use Modules\ERP\Entities\Transaction;
 use Modules\ERP\Entities\Workman;
+use Modules\ERP\Jobs\CourierCashJob;
 
 class ExpenseTransactionService 
 {
@@ -14,27 +15,32 @@ class ExpenseTransactionService
         $model = new Transaction();
         $model->amount = $request->amount;
         $model->description = $request->description;
-        $payer = User::find($request->receiver_id);
+        $payer = User::find($request->payer_id);
         switch ($purpose) {
             case 'lending':
                 $receiver = Workman::find($request->receiver_id);
                 $model->payer()->associate($payer);
                 $model->receiver()->associate($receiver);
                 $model->purpose()->associate($purpose);
+                $model->save();
+                CourierCashJob::dispatch($model);
                 break;
             case 'debt collection':
                 $receiver = Workman::find($request->payer_id);
                 $model->payer()->associate($payer);
                 $model->receiver()->associate($receiver);
                 $model->purpose()->associate($purpose);
+                $model->save();
+                CourierCashJob::dispatch($model);
                 break;
             default:
                 $model->payer()->associate($payer);
                 $model->purpose()->associate($purpose);
+                $model->save();
+                CourierCashJob::dispatch($model);
                 break;
         }
 
-        $model->save();
         return $model;
     }
 
