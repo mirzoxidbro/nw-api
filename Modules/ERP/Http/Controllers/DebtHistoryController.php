af24d4a79cf110b81273a19244ec8062d4b79213
mirzoxidbro <mirzoxidadxamjanov01@gmail.com>
2023-01-28 15:00:55 +0500

    debt history

---
+++ Modules/ERP/Http/Controllers/DebtHistoryController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Modules\ERP\Http\Controllers;
+
+use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Modules\ERP\Entities\DebtHistory;
+
+class DebtHistoryController extends Controller
+{
+   public function index()
+   {
+      return DebtHistory::query()->get();
+   }
+
+   public function store(Request $request)
+   {
+        $debthistory = new DebtHistory();
+        $debthistory->workman_id = $request->workman_id;
+        $debthistory->save();
+        return $debthistory;
+   }
+}
