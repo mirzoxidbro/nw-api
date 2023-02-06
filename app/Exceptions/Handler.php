e34c0717be5775d0c845a1cd9a8d98a93cb00828
mirzoxidbro <mirzoxidadxamjanov01@gmail.com>
2023-02-06 11:26:18 +0500

    config fixed

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,48 +3,94 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\Routing\Exception\RouteNotFoundException;
 use Throwable;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Auth\Access\AuthorizationException;
+
 
 class Handler extends ExceptionHandler
 {
     /**
-     * A list of exception types with their corresponding custom log levels.
-     *
-     * @var array<class-string<\Throwable>, \Psr\Log\LogLevel::*>
-     */
-    protected $levels = [
-        //
-    ];
-
-    /**
      * A list of the exception types that are not reported.
      *
-     * @var array<int, class-string<\Throwable>>
+     * @var array
      */
     protected $dontReport = [
         //
     ];
 
     /**
-     * A list of the inputs that are never flashed to the session on validation exceptions.
+     * A list of the inputs that are never flashed for validation exceptions.
      *
-     * @var array<int, string>
+     * @var array
      */
     protected $dontFlash = [
-        'current_password',
         'password',
         'password_confirmation',
     ];
 
     /**
-     * Register the exception handling callbacks for the application.
+     * Report or log an exception.
      *
+     * @param \Throwable $exception
      * @return void
+     *
+     * @throws \Exception
      */
-    public function register()
+    public function report(Throwable $exception)
     {
-        $this->reportable(function (Throwable $e) {
-            //
-        });
+        parent::report($exception);
+    }
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Throwable $exception
+     * @return \Symfony\Component\HttpFoundation\Response
+     *
+     * @throws \Throwable
+     */
+    public function render($request, Throwable $exception)
+    {
+        return $this->handleException($request, $exception);
+    }
+
+    public function handleException($request, Throwable $exception)
+    {
+        if ($exception instanceof MethodNotAllowedHttpException) {
+            return response()->errorJson('The specified method for the request is invalid', 405);
+        }
+
+        if ($exception instanceof RouteNotFoundException || $exception instanceof NotFoundHttpException) {
+            return response()->errorJson('The specified URL cannot be found', 404);
+        }
+
+        if ($exception instanceof HttpException) {
+            return response()->errorJson($exception->getMessage(), $exception->getStatusCode());
+        }
+
+        if ($exception instanceof ValidationException) {
+
+            $items = $exception->validator->errors()->getMessages();
+
+            return response()->errorJson($exception->getMessage(), 422, $items);
+        }
+
+        if ($exception instanceof AuthenticationException) {
+            return response()->errorJson($exception->getMessage(), 401);
+        }
+
+        if ($exception instanceof AuthorizationException) {
+            return response()->errorJson($exception->getMessage(), 403);
+        }
+
+        return response()->errorJson($exception->getMessage() . ' in ' . $exception->getFile() . ":" . $exception->getLine(), 500);
+
     }
 }
