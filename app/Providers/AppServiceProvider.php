40b5daaf2a7b1c1b6f0bcb49082500e9df54220e
mirzoxidbro <mirzoxidadxamjanov01@gmail.com>
2023-02-06 11:06:37 +0500

    ResponseFactoryResponeMixin

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Mixins\ResponseFactoryMixin;
+use Illuminate\Routing\ResponseFactory;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        ResponseFactory::mixin(new ResponseFactoryMixin());
     }
 }
