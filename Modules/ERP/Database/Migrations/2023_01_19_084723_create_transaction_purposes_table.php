9e374a82d7ec22d154097ab37f69b21e2ad2ab8d
mirzoxidbro <mirzoxidadxamjanov01@gmail.com>
2023-03-02 09:24:52 +0500

    fixed

--- Modules/ERP/Database/Migrations/2023_01_19_084723_create_transaction_purposes_table.php
+++ Modules/ERP/Database/Migrations/2023_01_19_084723_create_transaction_purposes_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('transaction_purposes', function (Blueprint $table) {
             $table->id();
-            $table->enum('type', ['income', 'expense', 'transfer']);
+            $table->string('type');
             $table->string('title');
             $table->boolean('canBeChanged')->default(0);
             $table->timestamps();
