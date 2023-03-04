347a9acb430fde0abb60fe9527bd922e1e040168
mirzoxidbro <mirzoxidadxamjanov01@gmail.com>
2023-03-04 15:23:55 +0500

    debt history

--- Modules/ERP/Http/Controllers/DebtHistoryController.php
+++ Modules/ERP/Http/Controllers/DebtHistoryController.php
@@ -8,12 +8,5 @@ use Modules\ERP\Entities\DebtHistory;
 
 class DebtHistoryController extends Controller
 {
-
-   public function store(Request $request)
-   {
-        $debthistory = new DebtHistory();
-        $debthistory->workman_id = $request->workman_id;
-        $debthistory->save();
-        return $debthistory;
-   }
+   
 }
---
+++ Modules/ERP/Http/Requests/DebtHistory/IndexRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Modules\ERP\Http\Requests\DebtHistory;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class IndexRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'sort_key' => 'nullable',
+            'sort_type' => 'required_with:sort_key',
+            'username' => 'nullable|string',
+            'page' => 'nullable|numeric',
+            'per_page' => 'nullable|numeric'
+        ];
+    }
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+}
