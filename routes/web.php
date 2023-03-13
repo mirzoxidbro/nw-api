001876e5abe2f47fdd13885cfddbcfdcb6c62c7d
mirzoxidbro <mirzoxidadxamjanov01@gmail.com>
2023-03-13 14:01:23 +0500

    redis install

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $visits = Redis::decr('visits');
+    return $visits;
 });
